3ddd136cf00edd825b307efdbd4bd2c1dab79ae7
Moo <mohhab@kth.se>
2017-12-30 02:44:22 +0100

    smol stuf

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,4 @@
+/*
 // Welcoming voice everytime one enters/refreshes site.
 // TODO: make the voice randomized.
 // voice list is loaded async to the page. An onvoicechanged event is fired when they are loaded
@@ -20,3 +21,31 @@ function speak() {
     window.speechSynthesis.speak(msg);
     console.log(text.value);
 }
+
+
+<div id="tts">
+  <form id="form" action="/action_page.php">
+      Text-to-speech: <input id="input" type="text" text="I will say anything" value="Your wish is my command"><br>
+      <input id="button" type="button" value="Speak!" onclick="speak();">
+  </form>
+
+  <p>Text to speech</p>
+</div>
+
+
+//TODO: add schedule using table/grid or something.
+//TODO: plan layout and content of page.
+//TODO: get a nice background image/color.
+<div id="lower2container">
+    <div id="left">
+
+    </div>
+
+
+    <div id="right">
+
+    </div>
+</div>
+<script src="app/app.js" type="text/javascript"></script>
+
+*/
